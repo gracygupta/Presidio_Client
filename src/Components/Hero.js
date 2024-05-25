761a8f62b075a1babbe4ce9fe9fc1c18f9ce4b6a
UTKARSH SINGH <usjadon19@gmail.com>
2024-05-25 19:29:20 +0530

    landing page

--- src/Components/Hero.js
+++ src/Components/Hero.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import {Link} from "react-router-dom";
 
 function Hero() {
     return (
@@ -10,12 +11,25 @@ function Hero() {
                 <p className="text-2xl text-white mb-8">
                     Discover the perfect place to call home.
                 </p>
-                <button className="bg-white text-blue-500 font-bold py-2 px-4 rounded">
-                    Start Exploring
-                </button>
+                {/*<button className="bg-white text-blue-500 font-bold py-2 px-4 rounded">*/}
+                {/*    Start Exploring*/}
+                {/*</button>*/}
+
+                {/* Call-to-Action Buttons */}
+                <div className="flex justify-center space-x-4">
+                    <Link to="/login" className="bg-white text-blue-500 font-bold py-2 px-4 rounded">
+                        Login
+                    </Link>
+                    <Link to="/signup" className="bg-white text-blue-500 font-bold py-2 px-4 rounded">
+                        Sign Up
+                    </Link>
+                </div>
             </div>
         </section>
     );
 }
+
+
+
 
 export default Hero;
