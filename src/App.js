e51ccb7d4c152e9c8dfbb4e9586f440228acea15
gracygupta <hzgracy@gmail.com>
2024-05-28 11:09:23 +0530

    updates

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Test from "./Components/TestComponents";
 import LandingPage from "./Pages/LandingPage";
 import Login from "./Pages/LoginPage";
 import SignUp from "./Pages/SignUpPage";
---
+++ src/Components/TestComponents.js
@@ -0,0 +1,7 @@
+import React from "react"; // You only need React for a basic component
+
+function Test() {
+  return <h1 className="text-7xl text-center text-blue-800">Hello world!</h1>;
+}
+
+export default Test;
